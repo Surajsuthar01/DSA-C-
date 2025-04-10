d2531734d08299970d050e89cdc57e38255cc955
Suraj Suthar <144941218+Surajsuthar01@users.noreply.github.com>
2025-04-10 13:47:51 +0530

    Update Asuraj1.cpp

--- Asuraj1.cpp
+++ Asuraj1.cpp
@@ -1,226 +1,226 @@
 #include<iostream>
 using namespace std;
 int main (){
-    // int s;
-    // cout<<"enter the value of s";  
-    // cin>>s;
-    //cout<<"suraj";?
-     // if (s>0){
-    //     cout<<"suraj learn c++";
-    // }
-    // else{
-    //     cout<<"suraj is not leaning";
-    // }
-    //  if(s>0){
-    //     cout<<"suraj learn c++"<<endl;
-    //  } else if (s=0){
-    //     cout<<"suraj are right"<<endl;
-    //  }else if (s<0){
-    //     cout<<"suraj is not learning"<<endl;
-    //  }
-//    int s;
-//    cin>>s;
-//    int i=1;
-//    while(i<=s){
-//     cout<<i<<" ";
-//     i++;
-//    }
-//  int n;
-//  cin>>n;
-//  int i=1;
-//  int sum =0;
-//  while(i<=n){
-//  sum=sum+i;
-//  cout<<sum<<endl;
-//  i++;
-//  }
-//  cout<<sum<<endl;
-// int n;
-// cin>>n;
-// int i=1;
-// while(i<n){
-//     if(i%2==0){
-//         cout<<"it is not prime"<<i<<endl;
-//     }else{
-//         cout<<"it is prime"<<i<<endl;
-//     } i++;
-// }
-// int n;
-// cin>>n;
-// int i=1; 
+    int s;
+    cout<<"enter the value of s";  
+    cin>>s;
+    cout<<"suraj";?
+     if (s>0){
+        cout<<"suraj learn c++";
+    }
+    else{
+        cout<<"suraj is not leaning";
+    }
+     if(s>0){
+        cout<<"suraj learn c++"<<endl;
+     } else if (s=0){
+        cout<<"suraj are right"<<endl;
+     }else if (s<0){
+        cout<<"suraj is not learning"<<endl;
+     }
+   int s;
+   cin>>s;
+   int i=1;
+   while(i<=s){
+    cout<<i<<" ";
+    i++;
+   }
+ int n;
+ cin>>n;
+ int i=1;
+ int sum =0;
+ while(i<=n){
+ sum=sum+i;
+ cout<<sum<<endl;
+ i++;
+ }
+ cout<<sum<<endl;
+int n;
+cin>>n;
+int i=1;
+while(i<n){
+    if(i%2==0){
+        cout<<"it is not prime"<<i<<endl;
+    }else{
+        cout<<"it is prime"<<i<<endl;
+    } i++;
+}
+int n;
+cin>>n;
+int i=1; 
 
-// while(i<=n){
-//    int j=1;
-//     while(j<=n){
-//         cout<<" * ";
-//       j++;      
-//     } cout<<endl;
-//     i++;
-// }
-// int n;
-// cin>>n;
-// int i=1;
-// int count =1;
-// while(i<=n){
-//     int j=1;
-//     while(j<=n){
-//        cout<<count<<" ";
-//        count++;
-//        j++;
-//     }
-//     cout<<endl;
-//     i++;
-// }
+while(i<=n){
+   int j=1;
+    while(j<=n){
+        cout<<" * ";
+      j++;      
+    } cout<<endl;
+    i++;
+}
+int n;
+cin>>n;
+int i=1;
+int count =1;
+while(i<=n){
+    int j=1;
+    while(j<=n){
+       cout<<count<<" ";
+       count++;
+       j++;
+    }
+    cout<<endl;
+    i++;
+}
 
-// int n;
-// cin>>n;
-// int row=1;
-// int count=1;
-// while(row<=n){ 
-//     int col=1;
-//     while(col<=row){
-//         cout<<row+1<<" ";
-//         col++; count++;
-//     }
-//     cout<<endl;
+int n;
+cin>>n;
+int row=1;
+int count=1;
+while(row<=n){ 
+    int col=1;
+    while(col<=row){
+        cout<<row+1<<" ";
+        col++; count++;
+    }
+    cout<<endl;
    
-//     row++;
+    row++;
 
-// }
+}
 
-// int n;
-// cin>>n;
-// int row=1;
-// while(row<=n){
-//     int col=1;
-//      int value=row;
-//     while(col<=row){
-//       cout<<value<<" ";
-//       value=value+1;
-//       col++;
-//     }
-//     cout<<endl;
-//     row++;
-// }
+int n;
+cin>>n;
+int row=1;
+while(row<=n){
+    int col=1;
+     int value=row;
+    while(col<=row){
+      cout<<value<<" ";
+      value=value+1;
+      col++;
+    }
+    cout<<endl;
+    row++;
+}
 
 
-// int n;
-// cin>>n;
-// int row=1;
-// while(row<=n){
-//     int col=1;
-//     while(col<=row){
-//         cout<<row-col+1<<" ";
-//         col++;
-//     }cout<<endl;
-//     row++;
-// }
+int n;
+cin>>n;
+int row=1;
+while(row<=n){
+    int col=1;
+    while(col<=row){
+        cout<<row-col+1<<" ";
+        col++;
+    }cout<<endl;
+    row++;
+}
 
-// int n;
-// cin>>n;
-// int row=1;
-// while(row<=n){
-//     int col=1;
-//     while(col<=n){
-//         char ch= 'A'+row-1;
-//         cout<<ch;
-//         col++;
-//     }
-//     cout<<endl;
-//     row++;
-// }
+int n;
+cin>>n;
+int row=1;
+while(row<=n){
+    int col=1;
+    while(col<=n){
+        char ch= 'A'+row-1;
+        cout<<ch;
+        col++;
+    }
+    cout<<endl;
+    row++;
+}
 
-// int n;
-// cin>>n;
-// int row=1;
-// int counter=1;
-// while(row<=n){
-// int col=1;
-// while(col<=n){
-//   char ch = 'A'+ counter-1;
-//   cout<<ch<<" ";
-//   counter++;
-//   col++;
-// } cout<<endl;
-// row++;
-//}
+int n;
+cin>>n;
+int row=1;
+int counter=1;
+while(row<=n){
+int col=1;
+while(col<=n){
+  char ch = 'A'+ counter-1;
+  cout<<ch<<" ";
+  counter++;
+  col++;
+} cout<<endl;
+row++;
+}
 
-// int n;
-// cin>>n;
-// int row =1;
-// while(row<=n){
-//   int col=1;
+int n;
+cin>>n;
+int row =1;
+while(row<=n){
+  int col=1;
 
-//   while(col<=n){
-//     char ch ='A'+row +col -2;
-//     cout<<ch<<" "; 
-//     col++;
-//   }
-//   cout<<endl;
-//   row++;
-// }
+  while(col<=n){
+    char ch ='A'+row +col -2;
+    cout<<ch<<" "; 
+    col++;
+  }
+  cout<<endl;
+  row++;
+}
 
-// int n;
-// cin>>n;
-// int row=1;
-// while(row<=n){
-//   int col=1;
-//   while(col<=row){
-//     char ch = 'A'+row-1;
-//     cout<<ch<<" ";
-//     col++;
-//   }cout<<endl;
-//   row++;
-// }
+int n;
+cin>>n;
+int row=1;
+while(row<=n){
+  int col=1;
+  while(col<=row){
+    char ch = 'A'+row-1;
+    cout<<ch<<" ";
+    col++;
+  }cout<<endl;
+  row++;
+}
 
-// int n;
-// cin>>n;
-// int row=1;
-// while(row<=n){
-//   //space print 
-//   int space =n-row;
-//   while(space){
-//     cout<<" ";
-//     space--;
-//   }
-//   int col=1;
-//   while(col<=row){
-//     cout<<"*";
-//     col++;
-//   } cout<<endl;
-//   row++;
+int n;
+cin>>n;
+int row=1;
+while(row<=n){
+  //space print 
+  int space =n-row;
+  while(space){
+    cout<<" ";
+    space--;
+  }
+  int col=1;
+  while(col<=row){
+    cout<<"*";
+    col++;
+  } cout<<endl;
+  row++;
   
-// }
+}
 
-// int n;
-// cin>>n;
-// int row=1;
-// while(row<=n){
-// //print the space 
-// int space=n-row;
-// while(space){
-//   cout<<" ";
-//   space = space-1;
-// }
-// int col=1;
-// while(col<=row){
-//   cout<<col;
-//   col++;
-// }
-// int start=row-1;
-// while(start){
-// cout<<start;
-// start --;
-// }
-// cout<<endl;
-// row++;
-//   }
+int n;
+cin>>n;
+int row=1;
+while(row<=n){
+//print the space 
+int space=n-row;
+while(space){
+  cout<<" ";
+  space = space-1;
+}
+int col=1;
+while(col<=row){
+  cout<<col;
+  col++;
+}
+int start=row-1;
+while(start){
+cout<<start;
+start --;
+}
+cout<<endl;
+row++;
+  }
 
-// 4
-//    1
-//   121
-//  12321
-// 1234321
+4
+   1
+  121
+ 12321
+1234321
 
 
 
