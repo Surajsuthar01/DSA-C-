9b39942e1e2360111d96fbf32d9ad7f7c47f9803
Suraj Suthar <144941218+Surajsuthar01@users.noreply.github.com>
2025-04-10 13:53:12 +0530

    Update Asuraj2.cpp

--- Asuraj2.cpp
+++ Asuraj2.cpp
@@ -1,101 +1,101 @@
 //operators
 #include<iostream>
 using namespace std;
-// int main(){
-    // int a=3;
-    // int b=5;
-    // cout<<" a&b "<<(a&b)<<endl;
-    // cout<<" a | b  "<<(a|b)<<endl;
-    // cout<<" ~b   "<<(~b)<<endl; //~=not operator
-    // cout<<" a ^ b  "<<(a^b)<<endl;// ^ exor operator
+int main(){
+    int a=3;
+    int b=5;
+    cout<<" a&b "<<(a&b)<<endl;
+    cout<<" a | b  "<<(a|b)<<endl;
+    cout<<" ~b   "<<(~b)<<endl; //~=not operator
+    cout<<" a ^ b  "<<(a^b)<<endl;// ^ exor operator
 
-    // int n;
-    // cin>>n;
-    // for(int i=0;i<n;i++){
-    //     cout<<i<<" ";
-    // }
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<i<<" ";
+    }
 
-//     int n;
-//     cin>>n;
-//    int sum =0;
-//     for(int i=1;i<=n;i++){
+    int n;
+    cin>>n;
+   int sum =0;
+    for(int i=1;i<=n;i++){
        
-//       sum+=i;  
-//     }
-//     cout<<sum;
+      sum+=i;  
+    }
+    cout<<sum;
 
-//fibannaci series
-// int n;
-// cin>>n;
-// int a=0;
-// int b=1;
-// cout<<a<<" ";
-// for(int i=0;i<=n;i++){
-//     int nextnumber=a+b;
-//      cout<<nextnumber<<" ";
-//     a=b;
-//     b=nextnumber;
+fibannaci series
+int n;
+cin>>n;
+int a=0;
+int b=1;
+cout<<a<<" ";
+for(int i=0;i<=n;i++){
+    int nextnumber=a+b;
+     cout<<nextnumber<<" ";
+    a=b;
+    b=nextnumber;
   
-// }
+}
 
-//prime numbers
-// int n;
-// cin>>n;
-// for(int i=2;i<n;i++){
-//     if (n%i==0){
-//         cout<<"number is not prime "<<endl;
-//         break;
-//     }else{
-//         cout<<"prime number ";
-//         break;
-//     }
-// }
+prime numbers
+int n;
+cin>>n;
+for(int i=2;i<n;i++){
+    if (n%i==0){
+        cout<<"number is not prime "<<endl;
+        break;
+    }else{
+        cout<<"prime number ";
+        break;
+    }
+}
 
-// int n;
-// cin>>n;
-// bool isprime =1;
-// for(int i=2;i<n;i++){
-//     if(n%i==0){
-//         isprime=0;
-//     }
-// }
-// if(isprime == 0){
-//     cout<<"number is not prime ";
-// }else{
-//     cout<<"number is prime";
-// }
+int n;
+cin>>n;
+bool isprime =1;
+for(int i=2;i<n;i++){
+    if(n%i==0){
+        isprime=0;
+    }
+}
+if(isprime == 0){
+    cout<<"number is not prime ";
+}else{
+    cout<<"number is prime";
+}
 
-// int n;
-// cin>>n;
-// for(int i=1;i<n;i++){
-//     cout<<"hii"<<endl;
-//     cout<<"hey"<<endl;
-//     continue;
-//     cout<<"not reachable";
-// }
+int n;
+cin>>n;
+for(int i=1;i<n;i++){
+    cout<<"hii"<<endl;
+    cout<<"hey"<<endl;
+    continue;
+    cout<<"not reachable";
+}
 
-// for(int i=0;i<5;i++){
-//     for(int j=i;j<=5;j++){
-//          if (i+j == 10){
-//             break;
-//          }
-//          cout<<i<<" "<<j<<endl;
-//     }
-// }
+for(int i=0;i<5;i++){
+    for(int j=i;j<=5;j++){
+         if (i+j == 10){
+            break;
+         }
+         cout<<i<<" "<<j<<endl;
+    }
+}
 
-//operator precedence 
-// int subtractionproduct(int n) {
-//     int prod = 1;
-//     int sum = 0;
+operator precedence 
+int subtractionproduct(int n) {
+    int prod = 1;
+    int sum = 0;
     
-//     while (n != 0) {
-//         int digit = n % 10;
-//         prod = prod * digit;
-//         sum = sum + digit;
-//         n = n / 10;
-//     }
+    while (n != 0) {
+        int digit = n % 10;
+        prod = prod * digit;
+        sum = sum + digit;
+        n = n / 10;
+    }
     
-//     int answer = prod - sum;
-//     return answer;
-// }
+    int answer = prod - sum;
+    return answer;
+}
 
